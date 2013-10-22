b51d2fe09f22f2aea201d635cf4b8893bc9b0b49
xavier geerinck <thebillkidy@hotmail.com>
2013-10-22 12:08:27 +0200

    Init

---
+++ Databanken & Middleware/Examen/Java/src/com/desple/model/Artiest.java
@@ -0,0 +1,36 @@
+package com.desple.model;
+
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Xavier
+ * Date: 17/09/13
+ * Time: 12:05
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name="artiest")
+public class Artiest {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name="title")
+    private String title;
+
+    @OneToMany
+    @Cascade({org.hibernate.annotations.CascadeType.SAVE_UPDATE})
+    @JoinColumn(name="albumId")
+    private Set<Song> songs = new HashSet<Song>();
+
+    public Artiest() {
+    }
+
+
+}
