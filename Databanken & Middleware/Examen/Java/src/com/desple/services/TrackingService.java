c5c67c8d6ad3f48ba4acd687352db05d352e0e05
xavier geerinck <thebillkidy@hotmail.com>
2013-10-26 15:49:09 +0200

    Added services for the different things in DEEL 2 of exam

---
+++ Databanken & Middleware/Examen/Java/src/com/desple/services/TrackingService.java
@@ -0,0 +1,14 @@
+package com.desple.services;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Xavier
+ * Date: 26/10/13
+ * Time: 15:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class TrackingService {
+    public static void savePassage() {
+
+    }
+}
---
+++ Databanken & Middleware/Examen/Java/src/com/desple/services/VerkoopService.java
@@ -0,0 +1,14 @@
+package com.desple.services;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Xavier
+ * Date: 26/10/13
+ * Time: 15:44
+ * To change this template use File | Settings | File Templates.
+ */
+public class VerkoopService {
+    public static void registerOrder() {
+
+    }
+}
