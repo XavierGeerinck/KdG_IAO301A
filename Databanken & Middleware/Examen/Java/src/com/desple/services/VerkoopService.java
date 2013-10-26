6fd5c6eb9352cb48e1de17dcd1d3aa67bce44837
xavier geerinck <thebillkidy@hotmail.com>
2013-10-26 16:30:26 +0200

    Added order tickets.

--- Databanken & Middleware/Examen/Java/src/com/desple/services/VerkoopService.java
+++ Databanken & Middleware/Examen/Java/src/com/desple/services/VerkoopService.java
@@ -1,5 +1,14 @@
 package com.desple.services;
 
+import com.desple.model.Ticket;
+import com.desple.util.HibernateUtil;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Xavier
@@ -8,7 +17,34 @@ package com.desple.services;
  * To change this template use File | Settings | File Templates.
  */
 public class VerkoopService {
-    public static void registerOrder() {
+    public static boolean registerOrder(ArrayList<Ticket> tickets) {
+        // Create session
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = null;
 
+        try {
+            // Start transaction
+            transaction = session.beginTransaction();
+
+            // Save all the tickets
+            for (Ticket ticket : tickets) {
+                session.save(ticket);
+            }
+
+            // Commit
+            transaction.commit();
+        } catch (HibernateException e) {
+            transaction.rollback();
+            e.printStackTrace();
+
+            // We had an error so return false
+            return false;
+        } finally {
+            // Do not forget to close the session
+            session.close();
+
+            // Return true on success
+            return true;
+        }
     }
 }
