e4b402a15e3d9576d6454e496edf04822be73a45
Thierry <thierry.van.loo.tvl@gmail.com>
2013-10-27 21:21:10 +0100

    hanges in model

--- Databanken & Middleware/Examen/Java/src/com/desple/model/KoperTypes.java
+++ Databanken & Middleware/Examen/Java/src/com/desple/model/KoperTypes.java
@@ -4,9 +4,9 @@ package com.desple.model;
  * Created with IntelliJ IDEA.
  * User: Thierry
  * Date: 27/10/13
- * Time: 15:23
+ * Time: 20:40
  * To change this template use File | Settings | File Templates.
  */
-public enum TicketTypes {
-    NORMAL, VIP, COMBI, PERS
+public enum KoperTypes {
+    PARTICULIER, BEDRIJF, PERS
 }
